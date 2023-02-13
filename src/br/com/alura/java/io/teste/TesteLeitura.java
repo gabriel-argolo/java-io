b05854e73303371ab4cfccefdd58d3865488d590
Gabriel Argolo <gabriel_argolo@outlook.com.br>
2023-02-13 16:36:29 -0300

    Criação da classe TesteLeitura

---
+++ src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,24 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class TesteLeitura {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		FileInputStream fi = new FileInputStream("Lorem.txt");
+		InputStreamReader is = new InputStreamReader(fi);
+		BufferedReader br = new BufferedReader(is);
+		
+		String linha = br.readLine();
+		
+		while(linha!=null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+		br.close();
+	}
+
+}
