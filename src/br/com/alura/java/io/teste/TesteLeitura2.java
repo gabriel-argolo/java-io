e887137f2b27b8acb0aac28a818cb8c8e6eb1944
Gabriel Argolo <gabriel_argolo@outlook.com.br>
2023-02-13 22:21:03 -0300

    Inserção do teste leitura 2

---
+++ src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -0,0 +1,29 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+	public static void main(String[] args) throws Exception{
+		Scanner scanner = new Scanner(new File("contas.csv"));
+		while (scanner.hasNextLine()) {			
+			String linha = scanner.nextLine();
+			//			System.out.println(linha);
+			Scanner linhaScanners = new Scanner(linha);
+			linhaScanners.useDelimiter(",");
+			linhaScanners.useLocale(Locale.US);
+
+			String tipoConta = linhaScanners.next();
+			int agencia = Integer.parseInt(linhaScanners.next());
+			int numero = Integer.parseInt(linhaScanners.next());
+			String titular = linhaScanners.next();
+			double saldo = Double.parseDouble(linhaScanners.next());
+			//			System.out.println(tipoConta + agencia + numero + titular + saldo);
+			String valorFormatado = String.format(new Locale ("pt","BR"),"%s - %04d-%08d, %20s: %.2f:", tipoConta, agencia, numero, titular, saldo);
+			System.out.println(valorFormatado);
+			linhaScanners.close();
+		}
+		scanner.close();
+	}
+}
